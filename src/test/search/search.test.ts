88f84dedcfa5a08b1c4ea2ad7b65ec5736a2b103
Bogdan Dolin <d.bogdan@inbox.ru>
2019-10-21 22:13:03 +0700

    Add test case for search functionality
    The idea is simple: run betterSearch inside `__simple__`  folder and compare the results to our `expected` files. If the result matches the expected result - test passes! Otherwise it fails.

---
+++ src/test/search/search.test.ts
@@ -0,0 +1,65 @@
+import * as vscode from "vscode";
+import { expect } from "chai";
+import { getSamplesLocation, read, runBetterSearch, delay } from '../helpers';
+
+
+/**
+ * NOTE: Ideally we would want to test every single command with
+ * `vscode.commands.executeCommand('commandToTest')` but vscode API lacking the ability to
+ * insert text to InputBox prompt, so let's test the functionality with helper function `runBetterSearch` for now.
+ */
+
+// TODO: test error cases
+// TODO: fix tests on unix-based systems (path separators issue)
+describe('search', () => {
+
+	// close active editor after each test
+	afterEach( async () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			await delay(5000);
+		}
+	});
+
+	// it.only('should throw error if query is empty or undefined', async () => {
+	// 	const location = getSamplesLocation('search');
+	// 	const result = await runBetterSearch({ query: '', location });
+	// 	expect(result).to.throw();
+	// });
+
+	it('should work with multiple extensions', async () => {
+		const location = getSamplesLocation('search');
+		const expected = read('search/results/expected-multiple-extensions.txt');
+		const result = await runBetterSearch({ query: 'foo', location });
+		expect(result).to.equal(expected);
+	});
+
+	it('should work with single extension', async () => {
+		const location = getSamplesLocation('search/js');
+		const expected = read('search/results/expected-single-extension.txt');
+		const result = await runBetterSearch({ query: 'foo', location });
+		expect(result).to.equal(expected);
+	});
+
+	it('should work with regex query', async () => {
+		const location = getSamplesLocation('search');
+		const expected = read('search/results/expected-regex-query.txt');
+		const result = await runBetterSearch({ query: 'foo|bar|Baz', location, queryRegex: true });
+		expect(result).to.equal(expected);
+	});
+
+	it('should work with context option set to 0', async () => {
+		const location = getSamplesLocation('search');
+		const expected = read('search/results/expected-context-0.txt');
+		const result = await runBetterSearch({ query: 'foo|bar|Baz', location, queryRegex: true, context: 0 });
+		expect(result).to.equal(expected);
+	});
+
+	it('should work with context option set to 5', async () => {
+		const location = getSamplesLocation('search');
+		const expected = read('search/results/expected-context-5.txt');
+		const result = await runBetterSearch({ query: 'foo', location, context: 5 });
+		expect(result).to.equal(expected);
+	});
+});
